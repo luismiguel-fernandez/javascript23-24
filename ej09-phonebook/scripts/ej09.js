a6249b08217f93b6a11235635f87737e7a7f650b
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2023-10-03 12:08:25 +0200

    ej09 propuesto

---
+++ ej09-phonebook/scripts/ej09.js
@@ -0,0 +1,6 @@
+let phonebook = [
+    { name: 'Luismi', number: '666333999'},
+    { name: 'Antonia', number: '555123456'},
+    { name: 'Hulk Hogan', number: '987654321'},
+    { name: 'Donald Trump', number: '666666666'}
+]
