ad5448ee466592e8a13767cfaa14abc618d848a4
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2023-10-20 10:17:32 +0200

    ej11 terminado

--- ej11-elegir-empresa/scripts/ej11.js
+++ ej11-elegir-empresa/scripts/ej11.js
@@ -143,11 +143,36 @@ function imprimirPreferencias() {
             localStorage.setItem("preferencias",JSON.stringify(preferencias))
         })
         //falta también insertar otros botones para otras acciones
+        //botón SUBIR
+        let newButtonUp = document.createElement("button")
+        newButtonUp.classList.add("btn","btn-info")
+        newButtonUp.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg>`
+        newTD4.append(newButtonUp)
+        newButtonUp.addEventListener("click",function(){
+            //bajar en el array = aumentar su índice
+            if (indice == 0) return
+            let temp = pref
+            preferencias[indice] = preferencias[indice-1]
+            preferencias[indice-1] = temp
+            localStorage.setItem("preferencias",JSON.stringify(preferencias))
+            //modificar TABLE
+            imprimirPreferencias()
+        })
+        //botón BAJAR
         let newButtonDown = document.createElement("button")
         newButtonDown.classList.add("btn","btn-info")
-        newButtonDown.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
-        </svg>`
+        newButtonDown.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg>`
         newTD4.append(newButtonDown)
+        //clic en botón Bajar (bajar la fila en tabla HTML)
+        newButtonDown.addEventListener("click",function(){
+            //bajar en el array = aumentar su índice
+            if (indice == preferencias.length - 1) return
+            let temp = pref
+            preferencias[indice] = preferencias[indice+1]
+            preferencias[indice+1] = temp
+            localStorage.setItem("preferencias",JSON.stringify(preferencias))
+            //modificar TABLE
+            imprimirPreferencias()
+        })
     })
 }
