4761cf2348148167022938e809216431ea397f87
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2023-12-01 13:24:24 +0100

    ej21 terminado y ej22 planteado

--- ej21-afinidadfilmica/scripts/ej21.js
+++ ej21-afinidadfilmica/scripts/ej21.js
@@ -36,8 +36,92 @@ buscadorR.addEventListener("keyup", function() {
     }
 })
 
-
 /* ************************************************************* */
 /*                    SOLUCIÓN DE LA IZQUIERDA                   */
 /* ************************************************************* */
 const buscadorL = document.querySelector("#buscadorL")
+const tipoL = document.querySelector("#divBuscadorL select")
+const divResultadosL = document.querySelector("#divResultadosL")
+const seleccionadosL = document.querySelector("#divSeleccionadosL tbody")
+
+const todosBotones = document.querySelectorAll("#divSeleccionadosL tfoot button")
+
+function addResultado(texto,tipo) {
+    let nuevoResul = document.createElement("DIV")
+    nuevoResul.textContent = texto
+    divResultadosL.append(nuevoResul)
+    nuevoResul.classList.add("resul")
+    nuevoResul.dataset.tipo = tipo
+}
+
+function buscar(patron) {
+    fetch("server/search.php?p=" + patron)
+//    fetch("https://192.168.32.100/dwec/1.Javascript/javascript23-24/ej21-afinidadfilmica/server/search.php?p=" + patron)
+    .then( resp => resp.json() )
+    .then( json => {
+        json.forEach( r => {
+            switch (tipoL.value) {
+                case "0": addResultado(r.texto,r.tipo); break
+                case "1": if (r.tipo == "tit") addResultado(r.texto,r.tipo); break
+                case "2": if (r.tipo == "act") addResultado(r.texto,r.tipo); break
+                case "3": if (r.tipo == "dir") addResultado(r.texto,r.tipo); break
+            }
+        })
+    } )
+}
+
+buscadorL.addEventListener("keyup",function(){
+    let patron = this.value.trim()
+    divResultadosL.innerHTML = ""
+    if (patron != "") buscar(patron)
+})
+
+tipoL.addEventListener("change",function(){
+    let patron = buscadorL.value.trim()
+    divResultadosL.innerHTML = ""
+    if (patron != "") buscar(patron)
+})
+
+divResultadosL.addEventListener("click",function(ev){
+    if (ev.target.classList.contains("resul")) {
+        let nuevoTR = seleccionadosL.insertRow()
+        let nuevoTD1 = nuevoTR.insertCell()
+        let nuevoTD2 = nuevoTR.insertCell()
+        nuevoTD2.textContent = ev.target.textContent
+        let nuevoCB = document.createElement("INPUT")
+        nuevoCB.type = "checkbox"
+        nuevoTD1.append(nuevoCB)
+        nuevoTR.dataset.tipo = ev.target.dataset.tipo
+    }
+})
+
+document.querySelector("#divSeleccionadosL thead input").addEventListener("change",function(){
+    let todosCB = seleccionadosL.querySelectorAll('input[type="checkbox"]')
+    todosCB.forEach( cb => cb.checked = this.checked )
+})
+
+todosBotones[3].addEventListener("click",function(){
+    let todasFilas = seleccionadosL.querySelectorAll("tr")
+    todasFilas.forEach( fila => {
+        let filaCB = fila.querySelector("input")
+        if (filaCB.checked) fila.remove()
+    })
+})
+
+todosBotones[0].addEventListener("click",function(){
+    marcarFilas("tit")
+})
+todosBotones[1].addEventListener("click",function(){
+    marcarFilas("act")
+})
+todosBotones[2].addEventListener("click",function(){
+    marcarFilas("dir")
+})
+
+function marcarFilas(tipo) {
+    let todasFilas = seleccionadosL.querySelectorAll("tr")
+    todasFilas.forEach( fila => {
+        if (fila.dataset.tipo == tipo)
+            fila.querySelector("input").checked = true
+    })
+}
